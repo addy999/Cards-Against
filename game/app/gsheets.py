2722843e037721028e7fa3288eb524b7419167e1
addy999 <jude.addy999@gmail.com>
2020-03-02 01:29:29 -0500

    added gsheets

---
+++ game/app/gsheets.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+############################## Setup sheets ##############################
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '1sDRVGlAFT78bRghqMufdSYVU9DcfU4eQB9NYrpXVsac'
+
+creds = None
+# The file token.pickle stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    flow = InstalledAppFlow.from_client_secrets_file(
+        'credentials.json', SCOPES)
+    creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+        
+############################## Helper funcs ##############################
+
+def get_range(spreadsheet_id, range):
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                range=range).execute()
+    values = result.get('values', [])
+
+    if not values:
+        raise ValueError("No values found.")
+    
+    return values
