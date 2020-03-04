8144652e467081e65ccbf71d336833814b971016
addy999 <jude.addy999@gmail.com>
2020-03-04 00:43:43 -0500

    misc

--- game/app/cards/game.py
+++ game/app/cards/game.py
@@ -1,6 +1,15 @@
+import shortuuid
+from .classes import *
 
 class GameSession:
     
-    def __init__(self):
+    def __init__(self, black_cards=CardSet, white_cards=CardSet, id_len=5):
+
+        self.black_cards = black_cards
+        self.white_cards = white_cards
+        self.id = shortuuid.ShortUUID().random(length=id_len)
+    
+    
+    
         
-        def 
+    
